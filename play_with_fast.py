0b6a01ad89e25064b95f1de5c97158bdd5358496
yongshan <57080732+yyswhsccc@users.noreply.github.com>
2024-05-07 10:13:23 +0800

    Update play_with_fast.py

--- play_with_fast.py
+++ play_with_fast.py
@@ -36,8 +36,8 @@ sheet = service.spreadsheets()
 def get_image_and_captions(id):
     # 构建视频文件名
     video_filename = f"{id:08d}.mp4"
-    video_url = f"http://localhost:8093/{video_filename}"
-
+    # video_url = f"http://localhost:8093/{video_filename}"
+    video_url = f"http://localhost/videos/{video_filename}"
     # 假设所有文件都存在并且没有附带的 caption
     return video_url, None
 
