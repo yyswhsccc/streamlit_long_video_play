43a7899e355fd7e7b43612556051ecd3e4071243
yongshan <57080732+yyswhsccc@users.noreply.github.com>
2024-05-07 10:46:09 +0800

    Update play_with_fast.py

--- play_with_fast.py
+++ play_with_fast.py
@@ -39,8 +39,8 @@ def get_image_and_captions(id):
     # video_url = f"http://localhost:8093/{video_filename}"
     
     # 使用 AWS 实例的公共 DNS
-    public_dns = "ec2-54-176-199-228.us-west-1.compute.amazonaws.com"
-    video_url = f"http://{public_dns}/videos/{video_filename}"
+    # public_dns = "ec2-54-176-199-228.us-west-1.compute.amazonaws.com"
+    video_url = f"http://54.176.199.228/videos/{video_filename}"
 
     # 假设所有文件都存在并且没有附带的 caption
     return video_url, None
